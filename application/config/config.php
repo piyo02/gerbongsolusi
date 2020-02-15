006cf6fcc5163273272a71eca07c0190d645299d
piyo02 <alzidni2000@gmail.com>
2020-02-15 23:45:09 +0800

    m

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gerbongsolusi/';
-// $config['base_url'] = 'https://gerbongsolusi.com/';
+// $config['base_url'] = 'http://localhost/gerbongsolusi/';
+$config['base_url'] = 'https://gerbongsolusi.com/';
 
 /*
 |--------------------------------------------------------------------------
